ad8a1a611a8082c5d1c7bada6a840d3ba412c367
clemgi0 <clement.giraudon2@gmail.com>
2024-05-02 11:14:36 +0200

    add a working map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,16 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
-  standalone: true,
-  imports: [],
   template: `
-    <p>
-      map works!
-    </p>
+    <div class="map-frame">
+      <div id="map"></div>
+    </div>
   `,
-  styleUrl: './map.component.css'
+  styleUrls: ['./map.component.css']
 })
-export class MapComponent {
+export class MapComponent implements AfterViewInit {
+  private map: L.Map | L.LayerGroup<any> | undefined;
 
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 45.782910, 4.875903 ],
+      zoom: 16
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+
+    var marker = L.marker([45.78387073445425, 4.8746414388750345]).addTo(this.map!);
+    marker.bindPopup("<b>K-Fêt!</b><br>Pop me").openPopup();
+
+    this.map?.on('click', this.onMapClick.bind(this));
+  }
+
+  onMapClick(e :L.LeafletMouseEvent) {
+    var popup = L.popup();
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(this.map as L.Map);
+  }
 }
